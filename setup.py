e4fbbbc5726bf714fa93ba3514e70e42f0589dd1
HENRIQUE ZONTA TRAMONTINA <1139226@atitus.local>
2026-06-10 19:26:02 -0300

    executavel

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-import os,time,random, pygame, pyttsx3, cx_Freeze
-from cx_Freeze import setup, Executable
-setup(
-    name="Rodolfinho 2.0",
-    version="1.0",
-    description="Jogo em Pygame",
-    executables=[Executable("main.py")])
+import cx_Freeze
+
+executaveis = [
+        cx_Freeze.Executable(
+            script="main.py",
+            icon="bases/rodolfo.ico",
+            target_name="Rodolfinho"
+        )
+]
+
+cx_Freeze.setup(
+    name = "Rodolfo",
+    options = {
+        "build_exe":{
+            "packages":["pygame"],
+            "include_files":["bases","recursos"]
+        }
+    }, executables = executaveis
+)
